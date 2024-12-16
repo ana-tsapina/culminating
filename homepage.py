8f4fbabd2ab40e3f8042c2d9f5964fed89248ee5
ada <lovelace@SEA-GSDGTW3.EDU.YRDSB.NET>
2024-12-16 09:22:28 -0500

    :

--- homepage.py
+++ homepage.py
@@ -1,5 +1,13 @@
-import streamlit as st
+import streamlit as st 
+
+st.title("what would you like to do? ")
 
 
-st.title("What would you like to do?")
+log = st.button("Log")
+
+
+
+
+settings = st.button("Settings")
+cal = st.button("Calendar")
 
