1501c48431895764d0c2461ac0c9c6ae162527c9
ada <lovelace@SEA-GSDGTW3.EDU.YRDSB.NET>
2024-12-16 08:57:52 -0500

    :D

---
+++ pages/Calendar.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+
+
+var calendar = new Calendar(calendarEl, {
+  timeZone: 'UTC',
+  events: [
+    {
+      id: 'a',
+      title: 'my event',
+      start: '2018-09-01'
+    }
+  ]
+})
+
+var event = calendar.getEventById('a') // an event object!
+var start = event.start // a property (a Date object)
+console.log(start.toISOString()) // "2018-09-01T00:00:00.000Z"
