e0560462d3e5dbe45b713c803e5f1857a17bbcc3
ana-tsapina <anastasia.t1223@gmail.com>
2024-12-17 08:42:46 -0500

    update

--- homepage.py
+++ homepage.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import numpy 
+from PIL import Image
 
 
+
+st.title("Amplify")
+
+
+st.page_link("pages/Log.py", label = ":gray-background[Log]", use_container_width = True)
+st.page_link("pages/Settings.py", label = ":gray-background[Settings]")  
+st.page_link("pages/Calendar.py", label = ":gray-background[Calendar]")  
+
+st.text_input("Enter your priority task: ")
+
+
+st.title("what would you like to do? ")
+
+
+log = st.button("Log") 
+settings = st.button("Settings")
+cal = st.button("Calendar")
