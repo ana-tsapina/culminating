290ad5b03f61560268f958d49322dbf8e1af6325
ana-tsapina <anastasia.t1223@gmail.com>
2024-12-28 10:33:36 -0500

    new

--- homepage.py
+++ homepage.py
@@ -4,10 +4,12 @@ st.title("what would you like to do? ")
 
 
 log = st.button("Log")
-
-
-
-
 settings = st.button("Settings")
 cal = st.button("Calendar")
 
+if log: 
+        st.switch_page("Log.py")
+if settings: 
+        st.switch_page("Settings.py")
+if Calendar: 
+        st.switch_page("Calendar.py")
