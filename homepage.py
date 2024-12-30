a2ae3a9ab6db8704cecc4239febd531bbd6adc1d
ana-tsapina <anastasia.t1223@gmail.com>
2024-12-29 23:09:32 -0500

    new

--- homepage.py
+++ homepage.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("What would you like to do? ")
 
-st.page_link("pages/Log.py", label = "One")
-st.page_link("pages/Calendar.py", label = "Calendar")
+st.page_link("pages/Log.py", label = "Log")
 st.page_link("pages/Settings.py", label = "Settings")
--- pages/Log.py
+++ pages/Log.py
@@ -1,3 +1,51 @@
 import streamlit as st 
+import sqlite3
 
-st.text_input("What are your priorities for today? ")
+conn = sqlite3.connect('to_do.db')
+c = conn.cursor()
+
+
+def data_entry(activity, due_date, type): 
+    c.execute('INSERT INTO progress (task, due_date, category) VALUES(?,?,?)', (task, due_date, category))
+    conn.commit()
+   
+def create_table(): 
+    conn 
+
+task = st.text_input("What task do you want to add?")
+due_date = st.text_input("When is it due?")
+category = st.radio("Category", ["Physiological", "Safety and Security", "Love and Belonging", "Self-Esteem", "Self-Actualization"])
+add = st.button('Add')
+
+if add: 
+    if task and due_date and category: 
+        data_entry(task, due_date, category)
+    else: 
+        st.warning("Please enter a name")
+
+
+if st.button("Show Tasks"): 
+    c.execute("Select * FROM progress")
+    rows = c.fetchall()
+
+
+
+
+
+
+# class ToDo: 
+#     def __init__(self, task): 
+#         self.task = task
+
+ 
+# connect = sqlite3.connect(tasks.sqlite)
+# c = connect.cursor()
+
+
+# c.execute('''
+#     INSERT INTO tasks (current)
+# ''')
+# connect.commit()
+
+# insert_data()
+
