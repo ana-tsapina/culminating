6b32edd9d8a2d770fcd17145548bdaa665ae0046
ada <lovelace@mba.local>
2024-12-30 15:47:36 -0500

    reorganize and cleanup file structure

--- pages/Log.py
+++ pages/Log.py
@@ -1,51 +1,55 @@
 import streamlit as st 
 import sqlite3
+from datetime import datetime
 
-conn = sqlite3.connect('to_do.db')
+
+def begin_connection():
+    conn = sqlite3.connect('tasks.db')
+    return conn 
+
+conn = begin_connection()
 c = conn.cursor()
 
-
-def data_entry(activity, due_date, type): 
-    c.execute('INSERT INTO progress (task, due_date, category) VALUES(?,?,?)', (task, due_date, category))
+def insert_data(task, due_date, category, priority):
+    conn = begin_connection()
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks (task, due_date, category, priority) VALUES (?,?,?,?)", (task, due_date, category, priority))
     conn.commit()
-   
-def create_table(): 
-    conn 
+    conn.close()
 
+def table_creation(): 
+    conn = begin_connection()
+    c = conn.cursor()
+    c.execute(
+        '''CREATE TABLE IF NOT EXISTS tasks ( 
+            task TEXT 
+            due_date INT 
+            category TEXT 
+            priority INTEGER
+        )''') 
+    conn.commit()
+    conn.close()
+
+
+table_creation()
+
+st.title("Task Organization")
+day = st.date_input("What day is it today? ")
 task = st.text_input("What task do you want to add?")
-due_date = st.text_input("When is it due?")
+due_date = st.time_input("When is it due?", step = 60)
 category = st.radio("Category", ["Physiological", "Safety and Security", "Love and Belonging", "Self-Esteem", "Self-Actualization"])
+priority = st.slider("How important is it to you?", min_value = 0, max_value = 5, value = 2)
 add = st.button('Add')
 
+
+#print sucess after adding 
+
+
 if add: 
-    if task and due_date and category: 
-        data_entry(task, due_date, category)
-    else: 
-        st.warning("Please enter a name")
+    insert_data(task, due_date, category, priority)
 
 
 if st.button("Show Tasks"): 
-    c.execute("Select * FROM progress")
+    c.execute("Select * FROM tasks")
     rows = c.fetchall()
-
-
-
-
-
-
-# class ToDo: 
-#     def __init__(self, task): 
-#         self.task = task
-
- 
-# connect = sqlite3.connect(tasks.sqlite)
-# c = connect.cursor()
-
-
-# c.execute('''
-#     INSERT INTO tasks (current)
-# ''')
-# connect.commit()
-
-# insert_data()
 
